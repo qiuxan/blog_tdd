d794864c5f46c539b86746222c2b291964be35e1
qiuxan <qiuxan@gmail.com>
2019-03-18 05:52:19 +0000

    Go Go Gadget Tailwind

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,18 +2,23 @@
 
 @section('content')
 
-    <div class="flex items-center mb-4">
-        <a href="/projects/create">Create New Project</a>
+    <div class="flex items-center mb-3">
+        <a href="/projects/create">New Project</a>
+    </div>
+
+    <div class="flex">
+        @forelse($projects as $project)
+            <div class="bg-white mr-4 rounded shadow w-1/3 p-5" style="height: 200px">
+                <h3 class="text-xl font-normal py-4">{{$project->title }}</h3>
+                <div class="text-grey">{{str_limit($project->description,100)}}</div>
+            </div>
+
+        @empty
+            <div>No project</div>
+
+        @endforelse
     </div>
 
 
-    <ul>
-        @forelse($projects as $project)
-
-            <li><a href="{{$project->path()}}">{{$project->title}}</a></li>
-        @empty
-            <li>No Project yet.</li>
-        @endforelse
-    </ul>
 
 @endsection
